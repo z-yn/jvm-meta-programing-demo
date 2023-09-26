d389edb641d54712e5b432fc1a9c30521cd238cb
邹亚男 <zouyanan@eccom.com.cn>
2023-09-26 14:46:02 +0800

    init submit

---
+++ annotation-processor-test/src/main/java/com/githu/alex/meta/test/TestGetterSetter.java
@@ -0,0 +1,12 @@
+package com.githu.alex.meta.test;
+
+import com.github.alex.meta.annotation.Getter;
+import com.github.alex.meta.annotation.Setter;
+
+@Getter
+@Setter
+public class TestGetterSetter {
+    private String name;
+
+    private Long age;
+}
